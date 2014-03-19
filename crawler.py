feee7c56d2db3e816d279be589780ca551aa5972
vipulchaskar <chaskar.vipul@gmail.com>
2014-03-19 17:20:26 +0530

    added page ranking algorithm

--- crawler.py
+++ crawler.py
@@ -1,5 +1,6 @@
-#usage: prompt> python crawler.py http://www.yourwebsitenamehere.com/somepage
-#Scans the input page to find most of the links present and does simple crawling.
+#usage: prompt> python crawler.py http://www.pagetocrawl.com search_keyword
+#Starts crawling from the seed page specified in sys.argv[1]. Makes an index of the scanned keywords.
+#Computes ranks of all scanned pages with an algorithm identical to PageRank.
 #Be cautious, crawlers can quickly go out of hand since web is an exhaustive resource to crawl.
 #Use it in a controlled environment where you have a limited set of interlinked pages.
 
@@ -49,28 +50,56 @@ def add_to_index(index,keyword,url):
 def crawl_web(seed):
     tocrawl = [seed]
     crawled = []
+    graph = {}
     index = {}
     while tocrawl:
         page = tocrawl.pop()
         if page not in crawled:
             content = get_page(page)
             add_page_to_index(index,page,content)
-            union(tocrawl, get_all_links(content))
+            outlinks = get_all_links(content)
+            graph[page] = outlinks
+            union(tocrawl, outlinks)
             crawled.append(page)
     #return crawled
-    return index
+    return index,graph
 
-def lookup(index,word):
+def compute_ranks(graph):
+        d = 0.8
+        numloops = 10
+
+        ranks = {}
+        npages = len(graph)
+        for page in graph:
+                ranks[page] = 1.0/npages
+
+        for i in range(0,numloops):
+                newranks = {}
+                for page in graph:
+                        newrank = (1-d)/npages
+                        for node in graph:
+                                if page in graph[node]:
+                                        newrank += ranks[node] * d / len(graph[node])
+                        newranks[page] = newrank
+                ranks = newranks
+        return ranks
+
+
+def lookup(index,ranks,word):
+	results = {}
 	if word in index:
-		return index[word]
+		for result in index[word]:
+			results[result] = ranks[result]
+		return results
 	else:
 		return None
 
-index = crawl_web('http://www.udacity.com/cs101x/index.html')
+index,graph = crawl_web(sys.argv[1])
+
+ranks = compute_ranks(graph)
+
+#print(ranks)
 #print(index)
-print(index['a'])
-#print(crawl_web(sys.argv[1]))
-
-#print_all_links(get_page(sys.argv[1]))
-#'http://www.udacity.com/cs101x/index.html'
-
+resultdict = lookup(index,ranks,sys.argv[2])
+print(sorted(resultdict, key=resultdict.get, reverse=True))
+#http://www.udacity.com/cs101x/index.html
